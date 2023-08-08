6eb7c098ed6893df1b048efa4e5ca00bc4202cc3
HanYolo <simon.herzog@protonmail.ch>
2023-08-08 19:17:57 +0200

    google sign in working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	export let data
+
+	$: ({ supabase, session } = data)
+
+	const handleGoogleSignIn = async () => {
+		await supabase.auth.signInWithOAuth({
+			provider: 'google'
+		})
+	}
 </script>
 
 <h1>Bonjour</h1>
@@ -7,7 +15,7 @@
 {#if !data.session}
 	<a href="/login" role="button" style="width: 100%; margin-bottom: 1em">Login</a>
 	<a href="/register" role="button" style="width: 100%; margin-bottom: 1em">Register</a>
-	<button> Sign in with Google </button>
+	<button on:click={handleGoogleSignIn}> Sign in with Google </button>
 {:else}
 	<p>You are logged in!</p>
 {/if}
